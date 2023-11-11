7b839c9b161c6a0f49d9a8892614b150e0f85d8d
Job Krittapak <87260161+jktpt@users.noreply.github.com>
2023-11-12 00:25:46 +0700

    new 11

--- client/src/page/Form.jsx
+++ client/src/page/Form.jsx
@@ -39,7 +39,7 @@ export const Form = () => {
     formData.append("cogdisease", cogdisease);
     formData.append("allerdrug", allerdrug);
     formData.append("detail", detail);
-    if(tel.length !== 10 && idcard.length !== 13){
+    if(tel.length !== 10 || idcard.length !== 13){
       alert("กรุณาใส่เบอร์โทรศัพท์หรือเลขบัตรประจำตัวประชาชนให้ถูกต้อง");
     }else{
       if (data) {
